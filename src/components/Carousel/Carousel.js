4c5ebed71eaba44b1b996d136d2e999c35cfce4e
donellgill <donell.gill@outlook.com>
2021-10-27 23:04:26 -0400

    added Carousel component logic

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,7 +1,50 @@
 import React from "react"
+import "./Carousel.css"
 
 function Carousel(){
 
+    const imgs = document.getElementById('imgs')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+const img = document.querySelectorAll('#imgs img')
+
+let idx = 0;
+let interval = setInterval(run, 2000)
+
+function run(){
+
+    idx++
+    changeImage()
+}
+
+function changeImage(){
+    if (idx > img.length -1) {
+        idx = 0;
+    }else if(idx < 0){
+        idx = img.length - 1 
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`
+}
+
+function resetInterval(){
+
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
+}
+
+rightBtn.addEventListener('click', () => {
+    idx++
+    changeImage()
+    resetInterval()
+})
+
+leftBtn.addEventListener('click', () => {
+    idx--
+    changeImage()
+    resetInterval()
+})
 
     return(
         <div className ="carousel">
@@ -9,22 +52,22 @@ function Carousel(){
             <div className ='image-box'>
                 <img src="https://images.unsplash.com/photo-1496721398107-986c99ba7126?
                     ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80" 
-                    alt="a haircut first-image">
+                    alt="a haircut first" />
             </div>
             <div className = 'image-box'>
                 <img src="https://images.unsplash.com/photo-1629189784191-9afdcbcb0398?
                     ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80" 
-                    alt="another haircut second-image">
+                    alt="another haircut second" />
             </div>
             <div className="image-box">
                 <img src="https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?
                     ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=495&q=80" 
-                    alt="yet another haircut third-image">
+                    alt="yet another haircut third" />
             </div>
             <div className="image-box">
                 <img src="https://images.unsplash.com/photo-1494302377477-915db5a8e609?
                     ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" 
-                    alt="still a haircut fourth-image">
+                    alt="still a haircut fourth" />
             </div>
         </div>
         <div className="button-container">
